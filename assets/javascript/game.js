a8c91f33eff3371df427edbb51e4194754529363
Paul <hoffsbox@gmail.com>
2018-05-10 13:46:38 -0400

    Almost Done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,52 +4,53 @@ var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 
 // Create variables for tracking wins/losses/guess
 
-var wins = "0";
-var losses = "0";
-var guessesLeft = "9";
-var guess = "0";
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessedLetter = [];
+var userGuess = null;
 
-// Create a variable for the computer choice 
+// Computer generates random letter 
 
-var computerChoice = alpha[Math.floor(Math.random() * alpha.length)];
+var randomLetter = alpha[Math.floor(Math.random() * alpha.length)];
+console.log("Computer Guess: " + randomLetter);
 
- console.log(computerChoice)
+// Event used to creates variable for user's guess
+// Subtract from guess
+// Add selected letter to list of guesses
+// Check to see if the random letter matches the user's guess
 
-// Create a variable for the user guesses 
-
-document.onkeyup = function(event) {
-    var userGuess = (event.key.toLowerCase());
+document.onkeypress = function(event) {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    guesses--;
+    guessedLetter.push(userGuess);
+    document.querySelector('#guessesLeft').innerHTML = "Guesses Left " + guesses;
+    document.querySelector('#guesses').innerHTML = "Guesses Letters: " + guessedLetter;
     console.log(userGuess);
+    console.log("Guesses left:" + guesses);
+    console.log(guessedLetter);
+
+    if (userGuess == randomLetter){
+        wins++;
+        alert("You win, but you knew that was going to happen... You're Psychic!");
+        guesses = 9;
+        guessedLetter.length  = 0;
+        randomLetter = alpha[Math.floor(Math.random() * alpha.length)];
+        document.querySelector('#wins').innerHTML = "Wins: " + wins;
+        document.querySelector('#guessesLeft').innerHTML = "Guesses Left " + guesses;
+        document.querySelector('#guesses').innerHTML = "Guesses Letters:" + guessedLetter + " ";
+
+    } else if(guesses == 0) {
+        losses++;
+        alert("You Lose, sorry you're no Pcychic.");
+        guesses = 9;
+        guessedLetter.length  = 0;
+        randomLetter = alpha[Math.floor(Math.random() * alpha.length)];
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        document.querySelector('#guessesLeft').innerHTML = "Guesses Left " + guesses;
+        document.querySelector('#guesses').innerHTML = "Guesses Letters: " + guessedLetter;
+    }
 }
 
-// Compare the user guess against the computer choice & incriment the win/loss/guesse variables
-
-
-
-
-// document.onkeyup = function() {
-//    var userguess = String.fromCharCode(event.keyCode).
-//    toLowerCase();
-//
-//    console.log(userguess);
-//
-//    var computerGuess = alpha[Math.floor(Math.random()*options.length)];
-//
-//    console.log(computerGuess);
-//
-//   if(userguess === computerGuess) {
-//       wins++;
-//   } else {
-//       losses++;
-//   }
-
-//    var html = <p>Press Any Letter Key to Begin</p> +
-//    "<p>Wins: " + wins + "</p>" +
-//    "<p>Loses: " + losees + "</p>" +
-//    "<p>Guess Left: "guessesLeft + "</P>"+
-//   "<p>Guesses: "guess + "</p>";
-//
-//    document.querySelector('#game').innerHTML = html;
-// }
-
+// Update HTML on index Page
 
